0006530590c4fa14b53b3c85d1f176b181bc14d7
Evan Ovadia <verdagon@gmail.com>
2022-03-08 00:13:51 -0500

    Initial commit

---
+++ build.sh
@@ -0,0 +1,18 @@
+
+VALEC_DIR="$1"
+if [ "$VALEC_DIR" == "" ]; then
+  echo "Please supply the bootstrapping valec directory."
+  echo "Example: ~/TheValeCompiler"
+  exit
+fi
+shift;
+
+STDLIB_DIR="$1"
+if [ "$STDLIB_DIR" == "" ]; then
+  echo "Please supply the stdlib directory."
+  echo "Example: ~/stdlib"
+  exit
+fi
+shift;
+
+$VALEC_DIR/valec build stdlib=$STDLIB_DIR/src geomancer=src domino=~/VDomino/src rocketvale=~/RocketVale/src rocketvale=~/RocketVale/src/native/rust/target/debug/librocketvale.a valejson=~/ValeJSON/src parseiter=~/ParseIter/src --output_dir build --region_override assist --llvm_ir true
